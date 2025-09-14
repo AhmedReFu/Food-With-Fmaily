733772b1ecd233fc5bb1bf37f4bfb756d422e9b0
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2025-09-15 00:52:41 +0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+export default function App() {
+  return (
+    <View className="flex justify-items-center align-items-center">
+      <Text className="text-4xl text-blue-500 font-bold">I love food Its help me more energetic
+      </Text>
+    </View>
+  );
+}
