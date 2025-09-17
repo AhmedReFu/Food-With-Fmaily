88e916a9dd2622f74353edc60b8ebe79e0fe0b22
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2025-09-18 01:55:16 +0600

    Atuhenticating UI Sign In and Sign Up Compelete fully functional but not working on wating setup appwrite api

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig={
     endpoint:process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform:"com.RFLLC.foodwithfamily",
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.DATABASE_ID,
+    userCollectionId: process.env.USER_COLLECTION_ID
 }
